5181b1ed7150b9c1564f9b180b5a326255f76fff
GiuProgramer <giulidiazp@gmail.com>
2021-12-08 11:49:33 -0300

    Changed the middlegare in other routes

--- src/controllers/Auth/AuthController.js
+++ src/controllers/Auth/AuthController.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const jwtMiddleware  = require('../../middlewares/jwt');
+const jwtMiddleware = require("../../middlewares/jwt");
 
-const login = require('./loginRoute');
-const register = require('./registerRoute');
+const login = require("./loginRoute");
+const register = require("./registerRoute");
 
 const router = express.Router();
 
-router.use(jwtMiddleware);
-
 router.post("/register", register);
-router.post("/login", login)
-router.get('/test_token', jwtMiddleware, (req, res) => res.send('Token is valid'));
+router.post("/login", login);
+router.get("/test_token", jwtMiddleware, (req, res) =>
+  res.send("Token is valid")
+);
 
 module.exports = router;
