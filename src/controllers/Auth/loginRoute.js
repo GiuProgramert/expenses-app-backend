bdc46e35e20df5026fdd2b794e86645dd86ea587
GiuProgramer <giulidiazp@gmail.com>
2021-12-08 11:44:52 -0300

    send the token when the credentials are correct

--- src/controllers/Auth/loginRoute.js
+++ src/controllers/Auth/loginRoute.js
@@ -1,6 +1,8 @@
 const User = require("../../models/User");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 const { Op } = require('sequelize');
+const { JWT_SECRET } = require('../../config/getEnv');
 
 module.exports = async (req, res) => {
   let errors = [];
@@ -21,9 +23,19 @@ module.exports = async (req, res) => {
     });
     if (user.length > 0) {
       const hashedPassword = user[0].dataValues.password;
+      const { id, username } = user[0].dataValues;
       const match = await bcrypt.compare(password, hashedPassword);
       if (match) {
-        return res.json({ token: 'adsdadasd' }).status(200);
+        const token = jwt.sign(
+          {
+            id,
+            username
+          },
+          JWT_SECRET,
+          { expiresIn: 720 * 60 }
+        )
+
+        return res.json({ token }).status(200);
       }
 
       errors.push({ msg: 'el usuario o la contraseña estan mal' }) 
